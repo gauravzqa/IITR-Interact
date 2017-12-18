ab95d3921aeb93099fb962b1de70321de97b846c
gaurav <gauravzqa@gmail.com>
2017-12-18 23:35:20 +0530

    adding the basic architecture for app

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connect home</title>
+	<link rel="stylesheet" type="text/css" href="homestyle.css">
+	<meta charset="utf-8" >
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+	<div class="head">
+		<h1><img src="mc.png"></h1>
+	</div>
+	<div class="wrapper">
+		<nav>
+			<ul>
+				<li><a href="home.php">Home</a></li>
+				<li><a href="#about" data-scroll="true" data-id="#about">About</a></li>
+				<li><a href="#Connect" data-scroll="true" data-id="#connect">Connect</a></li>
+				<li><a href="#Contact" data-scroll="true" data-id="#contact">Contact</a></li>
+				<li><a href="#Login">Login</a></li>
+			</ul>
+			<ul class="social">
+				<li ><a href="">Facebook</a></li>
+				<li ><a href="">Gmail</a></li>
+				<li ><a href="">Linkedin</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="image">
+		<img src="c3.png" width="100%" style="z-index: -1;">
+	</div>
+	<div class="About">
+		<h2 id="about">About Connect</h2>
+		<p class="about">The aim of the project is to make connection between the
+	newbies with the multiple groups present in the campus. It also aims at making
+	connection between the junta in no group or some group with the groups
+	involved junta. It is basically an in-campus Facebook and LinkedIn, where one
+	can know the involvement of the second or the third person in different groups
+	and also knows about his/her skills and achievement and can get in touch with
+	him/her whenever required.</p>
+	</div>
+
+	<div id="connect">
+		<img  class="lc" src="letsconnect.jpg">
+	</div>
+</body>
+</html>
