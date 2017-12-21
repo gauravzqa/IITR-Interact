cde0785c05790cc2b98faf0b510ff6e42a4cc81e
gaurav <gauravzqa@gmail.com>
2017-12-21 16:07:23 +0530

    "remove images"

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<div class="head">
-		<h1><img src="mc.png"></h1>
+		<h1><img src="images/mc.png"></h1>
 	</div>
 	<div class="wrapper">
 		<nav>
@@ -17,7 +17,7 @@
 				<li><a href="#about" data-scroll="true" data-id="#about">About</a></li>
 				<li><a href="#Connect" data-scroll="true" data-id="#connect">Connect</a></li>
 				<li><a href="#Contact" data-scroll="true" data-id="#contact">Contact</a></li>
-				<li><a href="#Login">Login</a></li>
+				<li><a href="login.php">Login</a></li>
 			</ul>
 			<ul class="social">
 				<li ><a href="">Facebook</a></li>
@@ -27,11 +27,12 @@
 		</nav>
 	</div>
 	<div class="image">
-		<img src="c3.png" width="100%" style="z-index: -1;">
+		<img src="images/c3.png" width="100%" style="z-index: -1;">
 	</div>
 	<div class="About">
 		<h2 id="about">About Connect</h2>
-		<p class="about">The aim of the project is to make connection between the
+		<p class="about">
+			The aim of the project is to make connection between the
 	newbies with the multiple groups present in the campus. It also aims at making
 	connection between the junta in no group or some group with the groups
 	involved junta. It is basically an in-campus Facebook and LinkedIn, where one
@@ -40,8 +41,23 @@
 	him/her whenever required.</p>
 	</div>
 
-	<div id="connect">
-		<img  class="lc" src="letsconnect.jpg">
+	<div class="LC">
+		<img  class="lc" src="images/letsconnect.jpg">
+		
 	</div>
+<div class="info">
+	<p class="am">About ME</p>
+	
+	<div class="blah">
+	<p class="intro">I am a 18 years old undergraduate student currently pursuing my B.Tech degree in the field of electronics and communication Engg. at one of the most prestigious institute of the country IIT Roorkee.</p>
+	<p class="">I am a keen tech. enthusiast who believe in creating and working in productive projects, who is of the notion </p>
+	</div>
+	<div class="notion">
+	<p>DREAM it !!</p>
+	<p>BELIEVE it !!</p>
+	<p>BUILD it !!</p>
+	</div>
+</div>
+
 </body>
 </html>
